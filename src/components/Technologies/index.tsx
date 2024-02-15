a8644b95d13d124b4be8c106c96561b075d29837
Fabio J Raminhuk <fabioraminhuk@gmail.com>
2024-02-14 21:12:07 -0300

    feat: 🎸 Add more technologies

--- src/components/Technologies/index.tsx
+++ src/components/Technologies/index.tsx
@@ -5,13 +5,17 @@ export default function Technologies() {
     return <div className="mt-36">
         <Title 
             title="Technologies" 
-            description="Discover my creative and innovative projects, spanning design to development, and see how I can add value to your next endeavor"
+            description="Discover my skills and experience in various technologies through my personal website, where I share insights and innovative solutions"
         />
 
-        <div className="flex flex-col gap-10">
+        <div className="flex flex-col gap-16">
             {technologiesData?.map((item, i) => (
-                <div key={i} className="rounded-lg bg-gradient-custom p-[1px]">
-                    <span>{item.title}</span>
+                <div key={i}>
+                    <div className="flex items-center justify-between pb-3">
+                        <h3 className="text-2xl font-semibold">{item.title}</h3>
+                        <p>{item?.score >= 90 ? 'Advanced' : item?.score >= 60 && item?.score < 90 ? 'Intermediate' : item?.score > 40 && item?.score < 60 ? 'Regular' : 'Beginner'}</p>
+                    </div>
+                    <span className="block h-5 rounded-full bg-gradient-custom shadow-md shadow-blue-500/50" style={{width: item.score+'%'}}></span>
                 </div>
             ))}
         </div>
--- src/components/Technologies/projectsData.ts
+++ src/components/Technologies/projectsData.ts
@@ -7,19 +7,43 @@ type technologiesProps = {
 
 export const technologiesData: technologiesProps[] = [
     {
-        title: 'JavaScript, TypeScript',
+        title: 'HTML, CSS, Sass, Less & Tailwind CSS',
         score: 100
         
     },
     {
-        title: 'JavaScript, TypeScript',
-        score: 100
+        title: 'JavaScript, TypeScript & jQuery',
+        score: 90
         
     },
     {
-        title: 'JavaScript, TypeScript',
-        score: 100
+        title: 'React, Next &  Zustand',
+        score: 70
         
+    },
+    {
+        title: 'Styled Components',
+        score: 70
+        
+    },
+    {
+        title: 'UI design in Figma',
+        score: 70
+        
+    },
+    {
+        title: 'Node',
+        score: 50
+        
+    },
+    {
+        title: 'PHP, Twig',
+        score: 50
+        
+    },
+    {
+        title: 'PostgreSQL, GraphQL & Prisma',
+        score: 30
     },
    
     
