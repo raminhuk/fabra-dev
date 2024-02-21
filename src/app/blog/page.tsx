f78a941aec61f2b5af688be6432b3f41d50de71f
Fabio J Raminhuk <fabioraminhuk@gmail.com>
2024-02-21 19:41:49 -0300

    fix: 🐛 separando api

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,13 +1,6 @@
 import Posts from '@/components/Blog'
-const NOTION_BLOG_ID = '5d15147b9b2a4bc0a7b15f184991263d'
+import { getAllPosts } from '@/utils/api/splitbeeApi'
 export type Post = { id: string; slug: string; title: string; date: string };
-
-export const getAllPosts = async () => {
-    const res = await fetch(`https://notion-api.splitbee.io/v1/table/${NOTION_BLOG_ID}`)
-    const posts = await res.json()
-    console.log(posts)
-    return posts
-}
 
 export default async function Blog() {
     const recentPosts: Post[] = await getAllPosts()
