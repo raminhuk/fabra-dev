21e730172a28bfca2cd063b409918b09fdd28aff
Fabio J Raminhuk <fabioraminhuk@gmail.com>
2024-02-23 19:54:39 -0300

    feat: 🎸 add revalidate api - teste

--- src/utils/api/splitbeeApi.ts
+++ src/utils/api/splitbeeApi.ts
@@ -1,13 +1,17 @@
 const NOTION_BLOG_ID = '5d15147b9b2a4bc0a7b15f184991263d'
 
 export const getAllPosts = async () => {
-    const res = await fetch(`https://notion-api.splitbee.io/v1/table/${NOTION_BLOG_ID}`)
+    const res = await fetch(`https://notion-api.splitbee.io/v1/table/${NOTION_BLOG_ID}`,
+        { next: { revalidate: 300 }}
+    )
     const posts = await res.json()
     return posts
 }
 
 export const getPosts = async (id: string) => {
-    const res = await fetch(`https://notion-api.splitbee.io/v1/page/${id}`)
+    const res = await fetch(`https://notion-api.splitbee.io/v1/page/${id}`,
+        { next: { revalidate: 300 } }
+    )
     const post = await res.json()
     return post
 }
