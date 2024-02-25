c25b4fa4db5633b102eddb23759d7725b6d02204
Fabio J Raminhuk <fabioraminhuk@gmail.com>
2024-02-25 18:11:24 -0300

    feat: 🎸 add sendGTMEvent

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,3 +1,4 @@
+import { sendGTMEvent } from '@next/third-parties/google'
 import Image from 'next/image'
 import Link from 'next/link'
 
@@ -13,7 +14,7 @@ export default function Banner(){
                         </h1>
                         <p className="max-w-2xl tracking-wide text-coldGrey max-xs:text-sm">Turning Ideas into Digital Reality: Fabio J.R, a Frontend Developer passionate about crafting elegant and functional interfaces to deliver exceptional user experiences.</p>
                         <div className="flex max-lg:hidden">
-                            <Link href="/" className="rounded-full bg-blue-500 bg-gradient-custom px-8 py-4 text-sm shadow-lg shadow-blue-500/50">Download CV</Link>
+                            <Link href="/" onClick={() => sendGTMEvent({ event: 'download_cv', value: 'Download CV' })} className="rounded-full bg-blue-500 bg-gradient-custom px-8 py-4 text-sm shadow-lg shadow-blue-500/50">Download CV</Link>
                         </div>
                     </div>
 
--- src/utils/api/splitbeeApi.ts
+++ src/utils/api/splitbeeApi.ts
@@ -2,7 +2,7 @@ const NOTION_BLOG_ID = '5d15147b9b2a4bc0a7b15f184991263d'
 
 export const getAllPosts = async () => {
     const res = await fetch(`https://notion-api.splitbee.io/v1/table/${NOTION_BLOG_ID}`,
-        { next: { revalidate: 300 }}
+        { next: { revalidate: 3000 }}
     )
     const posts = await res.json()
     return posts
@@ -10,7 +10,7 @@ export const getAllPosts = async () => {
 
 export const getPosts = async (id: string) => {
     const res = await fetch(`https://notion-api.splitbee.io/v1/page/${id}`,
-        { next: { revalidate: 300 } }
+        { next: { revalidate: 3000 } }
     )
     const post = await res.json()
     return post
