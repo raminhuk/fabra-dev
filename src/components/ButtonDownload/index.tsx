3aee4313d889e8fa7bc2c8ac09c721264cd3192a
Fabio J Raminhuk <fabioraminhuk@gmail.com>
2024-02-28 21:13:34 -0300

    feat: 🎸 event click GA

--- src/components/ButtonDownload/index.tsx
+++ src/components/ButtonDownload/index.tsx
@@ -1,13 +1,11 @@
 'use client'
-
-import { sendGTMEvent } from '@next/third-parties/google'
-
  
+import { sendGAEvent } from '@next/third-parties/google'
  
 export function ButtonDownload() {
     return (
         <div>
-            <button onClick={() => sendGTMEvent({ event: 'download_cv' })} className="rounded-full bg-blue-500 bg-gradient-custom px-8 py-4 text-sm shadow-lg shadow-blue-500/50"
+            <button onClick={() => sendGAEvent({ event: 'download_cv' })} className="rounded-full bg-blue-500 bg-gradient-custom px-8 py-4 text-sm shadow-lg shadow-blue-500/50"
             >
             Download CV
             </button>
