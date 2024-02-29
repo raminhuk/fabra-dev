856c98e9d96d5568a2e1647e9de564f53d75ade3
Fabio J Raminhuk <fabioraminhuk@gmail.com>
2024-02-28 21:08:55 -0300

    feat: 🎸 add event gtm

--- src/components/ButtonDownload/index.tsx
+++ src/components/ButtonDownload/index.tsx
@@ -1,7 +1,13 @@
+'use client'
+
+import { sendGTMEvent } from '@next/third-parties/google'
+
+ 
+ 
 export function ButtonDownload() {
     return (
         <div>
-            <button id="download_cv" className="rounded-full bg-blue-500 bg-gradient-custom px-8 py-4 text-sm shadow-lg shadow-blue-500/50"
+            <button onClick={() => sendGTMEvent({ event: 'download_cv' })} className="rounded-full bg-blue-500 bg-gradient-custom px-8 py-4 text-sm shadow-lg shadow-blue-500/50"
             >
             Download CV
             </button>
