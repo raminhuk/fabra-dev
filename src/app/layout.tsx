3885a6ba0cba7c86b45ca6ecdfec74d92eb3420f
Fabio J Raminhuk <fabioraminhuk@gmail.com>
2024-02-29 22:28:36 -0300

    feat: 🎸 adjust css and lang

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -48,7 +48,7 @@ export default function RootLayout({
     children: React.ReactNode
 }) {
     return (
-        <html lang="pt-br">
+        <html lang="en">
             <body className={poppins.className}>
                 <Header />
                 {children}
