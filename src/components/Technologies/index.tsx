1abc9f5f76186867dad7234ebfefdd7786aa039f
Fabio J Raminhuk <fabioraminhuk@gmail.com>
2024-03-01 19:15:06 -0300

    feat: 🎸 adjust effect tech

--- src/components/Technologies/index.tsx
+++ src/components/Technologies/index.tsx
@@ -11,8 +11,8 @@ export default function Technologies() {
             const elements = document.querySelectorAll<HTMLElement>('[data-effect="js-tilt"]')
             elements.forEach(element => {
                 VanillaTilt.init(element, {
-                    max: 5,
-                    speed: 200,
+                    max: 8,
+                    speed: 300,
                     glare: true,
                     'max-glare': 0.5,
                     scale: 1.05
@@ -25,8 +25,8 @@ export default function Technologies() {
         <div>
             <div className="relative grid grid-cols-3 flex-col gap-8 max-lg:grid-cols-2 max-lg:gap-4 max-md:grid-cols-1">
                 {technologiesData?.map(({title, icon, description}, i) => (
-                    <div key={i} data-effect="js-tilt" className="group flex cursor-pointer items-center gap-5 rounded-xl border-l-[1px] border-t-[1px] border-gray-700 bg-gray-700 !bg-opacity-30 p-6 backdrop-blur-[6px] transition-all">
-                        <div className="flex flex-col gap-1 transition-all">
+                    <div key={i} data-effect="js-tilt" className="group flex cursor-pointer items-center gap-5 rounded-xl border-l-[1px] border-t-[1px] border-gray-700 bg-gray-700 !bg-opacity-30 p-6 backdrop-blur-[6px]">
+                        <div className="flex flex-col gap-1">
                             <span className="mb-2 flex gap-3">
                                 {icon && (
                                     <span className="flex size-7 min-w-8">
