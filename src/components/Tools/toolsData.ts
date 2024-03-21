3d2d42cd5d510d7c23973a31152797954f5b3e01
Fabio J Raminhuk <fabioraminhuk@gmail.com>
2024-03-20 22:50:47 -0300

    feat: 🎸 remove data fake tools

--- src/components/Tools/toolsData.ts
+++ src/components/Tools/toolsData.ts
@@ -10,12 +10,4 @@ export const toolsData: toolsProps[] = [
         title: 'Border Radius Generator',
         description: 'Customize HTML element corners effortlessly with the Border Radius Generator. Get instant CSS code to add rounded, smooth, or angular borders to your website with ease.',
     },
-    {
-        title: 'Border Radius Generator',
-        description: 'Customize HTML element corners effortlessly with the Border Radius Generator. Get instant CSS code to add rounded, smooth, or angular borders to your website with ease.',
-    },
-    {
-        title: 'Border Radius Generator',
-        description: 'Customize HTML element corners effortlessly with the Border Radius Generator. Get instant CSS code to add rounded, smooth, or angular borders to your website with ease.',
-    },
 ]
