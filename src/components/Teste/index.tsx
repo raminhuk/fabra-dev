e2cee123484d1e69b24eed56ea984408c2b93eab
Fabio J Raminhuk <fabioraminhuk@gmail.com>
2024-04-06 13:33:57 -0300

    feat: 🎸 new page web stories

--- src/components/Teste/index.tsx
+++ src/components/Teste/index.tsx
@@ -1,7 +1,18 @@
-'use client'
-
-export function MyComponent() {
+export const config = { amp: true }
+ 
+function MyAmpPage() {
+    const date = new Date()
     return (
-        <></>
+        <div>
+            <p>Some time: {date.toJSON()}</p>
+            <amp-story standalone
+                title="Joy of Pets"
+                publisher="AMP tutorials"
+                publisher-logo-src="assets/AMP-Brand-White-Icon.svg"
+                poster-portrait-src="assets/cover.jpg">
+            </amp-story>
+        </div>
     )
 }
+ 
+export default MyAmpPage
---
+++ typings.d.ts
@@ -0,0 +1,10 @@
+declare namespace JSX {
+    interface IntrinsicElements {
+      'amp-timeago': any;
+      'amp-story': any;
+      'amp-story-page': any;
+      'amp-story-grid-layer': any;
+      'amp-img': any;
+      'amp-video': any;
+  }
+}
